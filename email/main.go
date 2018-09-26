bcf70cc6ff4d7c0e4ee34585648a77d27ad216af
AlfieriChou <alfierichou@gmail.com>
2018-09-26 22:05:56 +0800

    fibonacci

--- email/main.go
+++ email/main.go
@@ -13,6 +13,6 @@ func main() {
   e.Subject = "Awesome Subject"
   e.Text = []byte("Text Body is, of course, supported!")
   e.HTML = []byte("<h1>Fancy HTML is supported, too!</h1>")
-  e.Send("smtp.gmail.com:587", smtp.PlainAuth("", "alfierichou@gmail.com", "****", "smtp.gmail.com"))
-  fmt.Println("OK")
+  e.Send("smtp.gmail.com:587", smtp.PlainAuth("", "alfierichou@gmail.com", "*****", "smtp.gmail.com"))
+  fmt.Println("OK", e)
 }
