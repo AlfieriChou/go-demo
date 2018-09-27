6ab45c653226abdb34cce4300c1a557e4e71d6e9
AlfieriChou <alfierichou@gmail.com>
2018-09-27 11:34:58 +0800

    go typeof and slice split

--- slice/main.go
+++ slice/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
   "fmt"
+  "strings"
+  "reflect"
 )
 
 func main () {
   Append()
   Map()
+  Split()
 }
 
 func Append() {
@@ -20,4 +23,12 @@ func Map() {
   slice2 := make([]int, 2)
   copy(slice2, slice1)
   fmt.Println(slice1, slice2)
+}
+
+func Split() {
+  var a string = "a,b,c,d"
+  b := strings.Split(a, ",")
+  fmt.Println(reflect.TypeOf(b))
+  fmt.Println(reflect.TypeOf(a))
+  fmt.Println(b, a)
 }
